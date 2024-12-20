06d67a303e9975f46c1c0de770e47553867873f9
Nassim LOUNADI <39556046+nassimlnd@users.noreply.github.com>
2024-12-20 21:14:58 +0100

    feat(landing): header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        primary: "#101828",
+        tertiary: "#475467",
+        button: {
+          secondary: {
+            fg: "#344054"
+          }
+        }
+      },
+      borderColor: {
+        button: {
+          secondary: "#D0D5DD",
+        }
+      }
+    },
   },
   plugins: [],
 };
